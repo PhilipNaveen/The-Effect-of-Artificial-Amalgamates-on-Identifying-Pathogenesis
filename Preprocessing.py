cc2430ab7230fd3173772ced4adb33f50fd8cc60
PhilipNaveen <64291543+PhilipNaveen@users.noreply.github.com>
2021-06-19 16:11:43 -0400

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,20 +1,14 @@
-from skimage import measure
+ from skimage import measure
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy as nm
 import cv2
 import random
+import os
+import tensorflow as tf
 
 def mse(imageA, imageB):
-	# the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
-	# NOTE: the two images must have the same dimension
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
-	
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
-	return err
-
+    err = nm.sum((imageA.astype("float") - imageB.astype("float")) ** 2); err /= float(imageA.shape[0] * imageA.shape[1])
+    return err
 
 def compare_dataset(data):
     vals = []
@@ -22,19 +16,30 @@ def compare_dataset(data):
         vals.append(mse(point, data[random.randint(0, len(data)-1)]))
     return vals
 
-
 def gaussian_distribution(x, dataset):
     mean, sd = float(nm.mean(dataset)), float(nm.std(dataset))
     prob_density = (np.pi*sd) * np.exp(-0.5*((x-mean)/sd)**2)
     return prob_density
     
-def load(path, classification=None, x=[], y=[]):
+def load_images_and_labels(path, classification=None, x=[], y=[]):
     x, y = list(x), list(y)
     for subdir, dirs, files in os.walk(path):
         for file in files: 
-            x.append(nm.array(resize(cv2.imread(subdir + "\\" + file))).reshape(1, 100, 100, 3))
+            x.append(nm.array(resize(cv2.imread(subdir + "\\" + file))))
             y.append([classification])
     return nm.array(x), nm.array(y)
+
+def load_images(path, x=[]):
+    x = list(x)
+    for subdir, dirs, files in os.walk(path):
+        for file in files: 
+            x.append(nm.array(resize(cv2.imread(subdir + "\\" + file))))
+    return nm.array(x)
             
 def resize(image, IMG_SIZE=100):
     return cv2.resize(image, (IMG_SIZE, IMG_SIZE))
+
+def extract(model, images):
+    points = []
+    for image in images: points.append(model.predict(image.reshape(1, 100, 100, 3))[0][0])
+    return points
